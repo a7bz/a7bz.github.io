b7da67b14f020b37ced66ce1e64b969df082c8a0
a7bz <841105248@qq.com>
2024-10-17 18:24:00 +0800

    ✨ feat: 侧边栏组件添加新功能

--- .vitepress/theme/components/layout/Aside/index.vue
+++ .vitepress/theme/components/layout/Aside/index.vue
@@ -1,11 +1,66 @@
 <template>
     <aside class="main-aside">
-        <Intro />
+        <Intro class="weidgets" />
+        <div class="sticky">
+            <Toc v-if="toc" class="weidgets" />
+            <Clock class="weidgets" />
+            <HotTags class="weidgets" />
+        </div>
     </aside>
 </template>
 
 <script setup>
+import HotTags from './HotTags.vue'
 import Intro from './Intro.vue'
+import Toc from './Toc.vue'
+const props = defineProps({
+    toc: {
+        type: Boolean,
+        default: false
+    }
+})
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.main-aside {
+    padding-left: 1rem;
+    display: flex;
+    flex-direction: column;
+    animation: fade-up 0.6s 0.3s backwards;
+
+    .weidgets {
+        padding: 18px;
+        margin-bottom: 1rem;
+
+        :deep(.title) {
+            margin-bottom: 12px;
+            font-weight: bold;
+            display: flex;
+            align-items: center;
+            opacity: 0.75;
+
+            .iconfont {
+                opacity: 0.6;
+                margin-right: 6px;
+            }
+
+            .title-name {
+                opacity: 0.8;
+            }
+        }
+    }
+
+    .sticky {
+        position: sticky;
+        top: calc(60px + 1rem);
+
+        .weidgets {
+            animation: fade-up 0.6s 0.4s backwards;
+
+            &:last-child {
+                margin-bottom: 0;
+            }
+        }
+    }
+}
+</style>
