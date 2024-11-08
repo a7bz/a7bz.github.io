ffd002e04d31cb4a401b232257528efb8712836b
a7bz <2484068670@qq.com>
2024-11-08 09:03:49 +0800

    ✨ feat: 添加网站统计组件

--- .vitepress/theme/scripts/tool.js
+++ .vitepress/theme/scripts/tool.js
@@ -5,11 +5,47 @@
  */
 export const generateId = (fileName) => {
     // 将文件名转换为哈希值
-    let hash = 0;
+    let hash = 0
     for (let i = 0; i < fileName.length; i++) {
         hash = (hash << 5) - hash + fileName.charCodeAt(i)
     }
     // 将哈希值转换为正整数
     const numericId = Math.abs(hash % 10000000000)
     return numericId
+}
+
+/**
+ * 动态加载脚本
+ * @param {string} src - 脚本 URL
+ * * @param {object} option - 配置
+ */
+export const loadScript = (src, option = {}) => {
+    if (typeof document === "undefined" || !src) return false
+    // 获取配置
+    const { async = false, reload = false, callback } = option
+    // 检查是否已经加载过此脚本
+    const existingScript = document.querySelector(`script[src="${src}"]`)
+    if (existingScript) {
+        console.log("已有重复脚本")
+        if (!reload) {
+            callback && callback(null, existingScript)
+            return false
+        }
+        existingScript.remove()
+    }
+    // 创建一个新的script标签并加载
+    return new Promise((resolve, reject) => {
+        const script = document.createElement("script")
+        script.src = src
+        if (async) script.async = true
+        script.onload = () => {
+            resolve(script)
+            callback && callback(null, script)
+        }
+        script.onerror = (error) => {
+            reject(error)
+            callback && callback(error)
+        }
+        document.head.appendChild(script)
+    })
 }
